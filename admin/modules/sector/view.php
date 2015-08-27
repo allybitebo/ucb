ea870663322aedbe2012e0e41204e9c116745d95
Shio <inocentvicent@gmail.com>
2015-08-27 10:08:22 +0100

    Modify sector, systemuser, and school in administrator
    home button and backward button was added in list, add, view and edit
    files

--- admin/modules/sector/view.php
+++ admin/modules/sector/view.php
@@ -1,4 +1,30 @@
-
+<div class="container">
+	<div class="panel panel-primary">
+		<div class="panel-heading">
+			<div class="row">
+				<div class="col-xs-3"><p align="left"><a href="<?php echo WEB_ROOT;?>admin/index.php?page=2" class="btn btn-info btn-xsm"><span class="glyphicon glyphicon-home"></span>Home</a><p>
+				</div>
+				
+				<div class="col-xs-9">
+					<div class="col-xs-8">
+						<p ><strong><h5 align="right">
+						<?php echo  $_SESSION['ACCOUNT_FNAME'] . " " . $_SESSION['ACCOUNT_LNAME'];?></h5></strong></p>
+					</div>
+					<div class="col-xs-4">
+						<div class="col-xs-6">
+							<p align="left"><a href="<?php echo WEB_ROOT;?>admin/modules/sector/index.php" class="btn btn-info btn-xsm">
+								<span class="glyphicon glyphicon-step-backward"></span>Back
+								</a
+								</p>
+							</div>
+							<div class="col-xs-6">
+								<p align="right"><a href="<?php echo WEB_ROOT;?>admin/logout.php"
+								class="btn btn-info btn-xsm"><span class="glyphicon glyphicon-log-out"></span>Log out</a></p>
+							</div>
+						</div>
+					</div>
+				</div>
+		</div> 
 <?php 
 	$sector = new Sector();
 	$list = $sector->single_sector($_GET['sectorId']);
@@ -7,8 +33,6 @@
 	
 	<fieldset>
 		<legend>Sector Information</legend>
-		
-		
 		<div class="form-group" id="sector_id">
 			<div class="col-md-8">
 				<label class="col-md-4 control-label" for="sector_id">Sector Number </label>
@@ -50,7 +74,5 @@
 </div>
 
 </form>
-<!--	</div><!--End of well-->
-
 </div><!--End of container-->
 
