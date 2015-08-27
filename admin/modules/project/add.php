c304dd34275da396310bb63206ffe205ed6b6a5e
Shio <inocentvicent@gmail.com>
2015-08-27 08:01:07 +0100

    Modify project in administrator panel
    home button and back button was added in list, add, view and edit files

--- admin/modules/project/add.php
+++ admin/modules/project/add.php
@@ -2,11 +2,28 @@
 	<div class="panel panel-primary">
 		<div class="panel-heading">
 			<div class="row">
-				<div class="col-xs-3"><p ><strong><h5 align="left"><?php echo  $_SESSION['ACCOUNT_FNAME'] . " " . $_SESSION['ACCOUNT_LNAME']; ?></h5></strong></p></div>
-				<div class="col-xs-3"></div>
-				<div class="col-xs-3"></div>
-				<div class="col-xs-3"><p align="right"><a href="<?php echo WEB_ROOT; ?>admin/logout.php" class="btn btn-info btn-xsm"><span class="glyphicon glyphicon-log-out"></span>Log out</a></p></div>
-			</div>
+				<div class="col-xs-3"><p align="left"><a href="<?php echo WEB_ROOT;?>admin/index.php?page=2" class="btn btn-info btn-xsm"><span class="glyphicon glyphicon-home"></span>Home</a><p>
+				</div>
+				
+				<div class="col-xs-9">
+					<div class="col-xs-8">
+						<p ><strong><h5 align="right">
+						<?php echo  $_SESSION['ACCOUNT_FNAME'] . " " . $_SESSION['ACCOUNT_LNAME'];?></h5></strong></p>
+					</div>
+					<div class="col-xs-4">
+						<div class="col-xs-6">
+							<p align="left"><a href="<?php echo WEB_ROOT;?>admin/modules/project/index.php" class="btn btn-info btn-xsm">
+								<span class="glyphicon glyphicon-step-backward"></span>Back
+							</a
+							</p>
+						</div>
+						<div class="col-xs-6">
+						<p align="right"><a href="<?php echo WEB_ROOT;?>admin/logout.php"
+						 class="btn btn-info btn-xsm"><span class="glyphicon glyphicon-log-out"></span>Log out</a></p>
+					</div>
+					</div>
+				</div>
+				</div>
 		</div> 
 		<form action="controller.php?action=add" class="form-horizontal well span9" method="post">
 			<fieldset>
