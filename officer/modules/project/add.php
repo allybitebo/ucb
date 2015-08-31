cd63b38c98916c6c11db8048261edc3f5d393adb
Shio <inocentvicent@gmail.com>
2015-08-31 22:22:59 +0100

    modify panel head for project under officer
    home button and back button was modified

--- officer/modules/project/add.php
+++ officer/modules/project/add.php
@@ -4,12 +4,28 @@
 <div class="container">
 	<div class="panel panel-primary">
 		<div class="panel-heading">
-			<div class="row">
-				<div class="col-xs-3"><p ><strong><h5 align="left"><?php echo $_SESSION['ACCOUNT_FNAME']. " " .$_SESSION['ACCOUNT_LNAME']  ; ?></h5></strong></p></div>
-				<div class="col-xs-3"></div>
-				<div class="col-xs-3"><p align="right"><a href="<?php echo WEB_ROOT; ?>/officer/index.php?page=1" class="btn btn-info btn-xsm"><span class="glyphicon glyphicon-home"></span>&nbsp;Home</a></p></div>
-				<div class="col-xs-3"><p align="right"><a href="<?php echo WEB_ROOT; ?>officer/logout.php" class="btn btn-info btn-xsm"><span class="glyphicon glyphicon-log-out"></span>Log out</a></p></div>
-			</div>
+<div class="row">
+				<div class="col-xs-3"><p align="left"><a href="<?php echo WEB_ROOT;?>officer/index.php?page=1" class="btn btn-info btn-xsm"><span class="glyphicon glyphicon-home"></span>&nbsp;Home</a><p>
+				</div>
+				<div class="col-xs-9">
+					<div class="col-xs-8">
+						<p ><strong><h5 align="right">
+						<?php echo  $_SESSION['ACCOUNT_FNAME'] . " " . $_SESSION['ACCOUNT_LNAME'];?></h5></strong></p>
+					</div>
+					<div class="col-xs-4">
+						<div class="col-xs-6">
+							<p align="left"><a href="<?php echo WEB_ROOT;?>officer/modules/project/index.php?view=ProjectConsultant" class="btn btn-info btn-xsm">
+								<span class="glyphicon glyphicon-step-backward"></span>Back
+							</a
+							</p>
+						</div>
+						<div class="col-xs-6">
+						<p align="right"><a href="<?php echo WEB_ROOT;?>officer/logout.php"
+						 class="btn btn-info btn-xsm"><span class="glyphicon glyphicon-log-out"></span>Log out</a></p>
+					</div>
+					</div>
+				</div>
+				</div>
 		</div> 
 		<?php 
 			/*if($projectFrom == "school"){
