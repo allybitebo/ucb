e357d0d783d58bcc96fe77ea5daa003472a3a505
Shio <inocentvicent@gmail.com>
2015-12-27 12:16:26 +0000

    modify list file in subject
    change functionality from the list of all subjects to the list according
    to domain

--- admin/modules/subject/list.php
+++ admin/modules/subject/list.php
@@ -26,53 +26,21 @@
 				</div>
 		</div>
 		<div class="wells">
-			<h3 align="left"><legend>List of Subjects</legend></h3>
-			<form action="controller.php?action=list" Method="POST">  					
-				<table id="example" class="table table-striped" cellspacing="0">
+			<div class="row">
+		
+			<div class="col-lg-10 col-md-10 col-sm-10">
+				
+				<div class="panel panel-primary">
+					<br />
+					<div class="panel-heading"><h5><strong>Subjects in Domain</strong></h5></div>
+					<?php require_once("domain.php"); ?>
 					
-					<thead>
-						<tr>
-							<th>No.</th>
-							<th width="15%" align="left"><input type="checkbox" name="chkall" id="chkall" onclick="return checkall('selector[]');"> Select All</th>
-							<th>Subject name</th>
-							<th>Description</th>
-							<th>Edit</th>
-							<th>Details</th>
-						</tr>	
-					</thead>
-					<tbody>
-						<?php
-							$subject = new Subject();
-							$subjectList = $subject->listOfSubjects();
-							foreach ($subjectList as $list) {
-								echo '<tr>';
-								echo '<td width="5%" align="center"></td>';
-								echo '<td width="15%"><input type="checkbox" name="selector[]" id="selector[]" value="'.$list->subject_id. '"/>';
-								echo '<td width="25%" >'. $list->name.'</td>';
-								echo '<td width="35%" >'. $list->description.'</td>';
-								echo '<td width="10%" ><a href = "index.php?view=edit&id='.$list->subject_id.'" ><span class="glyphicon glyphicon-list-alt"> </span>  Edit</a></td>';
-								echo '<td><a href = "index.php?view=view&subjectId='.$list->subject_id.'" ><span class="glyphicon glyphicon-list-alt"> </span>  View</a></td>';
-								echo '</tr>';
-							}
-						?>
-					</tbody>				 
-				</table>
-				<?php 
-					if($_SESSION['ACCOUNT_TYPE']=='administrator'){
-						echo '
-						<div class="btn-group">
-						<a href="index.php?view=add" class="btn btn-default"><span class="glyphicon glyphicon-plus-sign"></span>  New</a>
-						<button type="submit" class="btn btn-default" name="delete"><span class="glyphicon glyphicon-trash"></span> Delete Selected</button>
-						</div>';
-						}elseif($_SESSION['ACCOUNT_TYPE']=='Officer') {
-						echo '
-						<div class="btn-group">
-						<a href="index.php?view=add" class="btn btn-default"><span class="glyphicon glyphicon-plus-sign"></span>  New</a>
-						</div>';
-					}
-					
-				?>
-				<br /><br />
-			</form>
+				</div>
+				<div class="col-lg-2 col-md-2 col-sm-2">
+		</div>
+			</div>
+		
+		</div>
+			
 		</div>
 	</div><!--End of container-->	
